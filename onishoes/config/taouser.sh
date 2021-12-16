d3b86be492e0c31bbe24447010c1c31718061626
hoangtruong156 <danghoangtruong156@gmail.com>
2021-12-16 13:46:44 +0700

    xoa

--- onishoes/config/taouser.sh
+++
@@ -1,9 +0,0 @@
-clear
-read -p “Nhap chuoi dinh danh user” ma
-read -p “Nhap so luong user can tao ” n
-while [ $n -gt 0 ]
-do
-useradd $ma$n
-echo “123456” | passwd $ma$n  —stdin
-n=$[ $n – 1 ]
-done
--- onishoes/view/main/dangxuat.php
+++
@@ -1,7 +0,0 @@
-<?php
- session_start(); 
-	if(isset($_SESSION['login'])){
-		unset($_SESSION['login']);
-		header('location:../../index.php');
-	}
-?>
